277c2aea1e04d321adfcf76c8dbbbcbe169f8757
Signity <sujata.k@signitysolutions.in>
2018-05-14 11:15:54 +0530

    Initial Commit

---
+++ EncryptionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptionDemo
+//
+//  Created by Sakshi on 14/05/18.
+//  Copyright © 2018 Sakshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EncryptionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EncryptionDemo
+//
+//  Created by Sakshi on 14/05/18.
+//  Copyright © 2018 Sakshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
